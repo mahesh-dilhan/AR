0101e591aed16c3e62eeb61cbd5afb83cb84f723
mahesh <mail.mahesh.lk@gmail.com>
2021-07-11 14:20:28 +0800

    heroku url

--- location-based-ar/README.js
+++ location-based-ar/README.js
@@ -1,6 +1,8 @@
 
 ### App requires GPS and Camera to detect location based on Foursquare . 
-### if you want to test then , pls change client secret - its free
+### if you want to deloy same, pls change client secret. - its free from Foursquare
+
+Below is the setup using my credentials 
 ```
 https://frozen-castle-74925.herokuapp.com/
 ```
